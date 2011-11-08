60507f6f78d549da0d3ef832684e6ed5e63540b0
okard <d4rkdragon@gmail.com>
2011-11-08 21:10:10 +0100

    initial import

---
+++ scripts/bash/post.sh
@@ -0,0 +1,23 @@
+
+#lib.sh has to be included before
+
+# Call pre build function
+pre_build
+
+# Call build function
+build
+
+# Call post build function
+post_build
+
+# Configure Complete Package
+package
+
+# write pkgbuild file
+write_pkgbuild
+
+# Call clean function
+clean
+
+# Exit Bash
+exit
